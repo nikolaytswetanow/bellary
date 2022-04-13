8adb8fed8947778ce4e03dd06604dab21adb95cf
nikolaytswetanow <nikolaytswetanow@students.softuni.bg>
2022-04-13 11:14:30 +0300

    new settings

--- framework_exam/settings.py
+++ framework_exam/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,14 +112,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = (
-    BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles',
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
