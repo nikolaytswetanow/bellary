46f23a7097330cae6d05bc0005251abfbb38abf7
nikolaytswetanow <nikolaytswetanow@students.softuni.bg>
2022-04-13 11:05:03 +0300

    new settings

--- framework_exam/settings.py
+++ framework_exam/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
